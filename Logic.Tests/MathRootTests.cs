ee1e14219c159ed68c6c50c72b47f14e5900ab46
zmitr-gen <zmitr-gen@yandex.ru>
2017-07-09 23:11:49 +0300

    All bugs was fixed.

---
+++ Logic.Tests/MathRootTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static Logic.MathRoots;
+
+namespace Logic.Tests
+{
+    [TestFixture]
+    public class MathRootTests
+    {
+
+        #region Newton's method tests
+
+        [TestCase(4, 2, 0.01, 2.00)]
+        [TestCase(459, 2, 0.01, 21.424285287298908)]
+        [TestCase(1941, 3, 0.01, 12.474080133232556)]
+        [TestCase(1945, 10, 0.01, 2.1325147877535002)]
+        [Category("Newton's method")]
+        public void NewtonRoot_AllOk_ReturnsRoot(double number, double n, double e, double expectedRes)
+        {
+            double res = NewtonRoot(number, n, e);
+
+            Assert.AreEqual(expectedRes, res);
+        }
+
+        [Category("Newton's method")]
+        [TestCase(4, 0, 0.01)]
+        [TestCase(459, 2, 1.5)]
+        [TestCase(-1941, 4, 0.01)]
+        [TestCase(0, -5, 0.01)]
+        public void NewtonRoot_IncorrectArguments_ThrowsArgumentException(double number, double n, double e)
+        {
+            double res;
+
+            Assert.Catch<ArgumentException>(() => res = NewtonRoot(number, n, e));
+        }
+
+        #endregion
+
+    }
+}
---
+++ Logic/MathRoots.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+
+    /// <summary>
+    /// Helps to compute math roots by various methods.
+    /// </summary>
+    public class MathRoots
+    {
+
+        #region Newton's roots
+
+        /// <summary>
+        /// Computes root of n degree from <paramref name="number"/> by Newton's method.
+        /// </summary>
+        /// <param name="number"> Number to root extracting. </param>
+        /// <param name="n"> Degree of root. </param>
+        /// <param name="e"> Accurancy of computing. </param>
+        /// <returns> Root of n degree from number. </returns>
+        /// <exception cref="ArgumentException">
+        /// Have a place:
+        /// 1. <paramref name="n"/> is less than 1.
+        /// 2. <paramref name="e"/> isn't belong to the interval [ 0, 1 ].
+        /// 3. <paramref name="n"/> is even and <paramref name="value"/> is less than zero.
+        /// </exception>
+        public static double NewtonRoot(double number, double n, double e)
+        {
+            bool flag1 = n < 1 || e <= 0 || e >= 1;
+            bool flag2 = (number < 0 && (n % 2 == 0));
+            if (flag1 || flag2)
+                throw new System.ArgumentException();
+
+            double x0 = number / n;
+            double x = ((n - 1) * x0 + number / Math.Pow(x0, n - 1)) / n;
+
+            while (Math.Abs(x - x0) > e)
+            {
+                x0 = x;
+                x = ((n - 1) * x0 + number / Math.Pow(x0, n - 1)) / n;
+            }
+
+            return x;
+        }
+
+        #endregion
+
+    }
+}
+
